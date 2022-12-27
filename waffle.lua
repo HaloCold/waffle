a4f8b862df2b78aad99149619e103a885ffe7ffe
Halocold <111649003+HaloCold@users.noreply.github.com>
2022-12-27 13:52:17 -0600

    Ported from halocold.cf

---
+++ waffle.lua
@@ -0,0 +1,62 @@
+--[[
+
+Waffle: Version 0.9.1-alpha
+
+[ Credits, Downloads, More: https://halocold.cf/waffle ]
+
+]]
+
+local waffle = {}
+
+local currentStations = {}
+
+function waffle.New(stationName)
+  if not stationName:IsA("String") then
+    print("[Waffle:err]: StationName must be a string (waffle.New function)")
+  end
+  if stationName:IsA("String") then
+    local stationInstance = Instance.new("RemoteEvent")
+    stationInstance.Name = "waffle-"..StationName
+    table.insert(currentStations, stationInstance)
+
+    if game.ReplicatedStorage:FindFirstChild("WaffleStations") then
+      stationInstance.Parent = ReplicatedStorage:WaitForChild("WaffleStations")
+    else
+      local stationsFolder = Instance.new("Folder")
+      stationsFolder.Name = "WaffleStations"
+      stationsFolder.Parent = game.ReplicatedStorage
+
+      stationInstance.Parent = stationsFolder
+    end
+    return stationInstance
+  end
+end
+
+function waffle:Find(StationName)
+  local result = table.find(currentStations, StationName)
+  if result then
+    return result
+  end
+end
+
+-- waffle.ClientSay(any, station(from waffle.New), sendTo(LocalScript)
+
+function waffle.ClientSay(any, station, sendTo)
+  if game.ReplicatedStorage:FindFirstChild(station) and table.find(currentStations, station) then
+    station:FireClient(sendTo, any)
+    return true
+  else
+    return false
+  end
+end
+
+-- waffle.ServerSay(any, station(from waffle.New), sendTo(LocalScript)
+
+function waffle.ServerSay(any, station)
+  if game.ReplicatedStorage:FindFirstChild(station) and table.find(currentStations, station) then
+    station:FireServer(any)
+    return true
+  else
+    return false
+  end
+end
